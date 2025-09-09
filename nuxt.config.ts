960ef71dca2a7d55a101731e140ae90c8b02a6f6
Leo <leojunebedeo@gmail.com>
2025-09-09 18:10:14 +0800

    Setup PWA

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,66 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/fonts', '@nuxt/icon']
+  modules: [
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@vite-pwa/nuxt'
+  ],
+  pwa: {
+    strategies: 'generateSW',
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Nuxt 3 PWA',
+      short_name: 'NuxtPWA',
+      theme_color: '#ffffff',
+      description: 'Nuxt 3 PWA Application',
+      background_color: '#ffffff',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable'
+        }
+      ]
+    },
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+      runtimeCaching: [
+        {
+          urlPattern: '/',
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'page-cache'
+          }
+        },
+        {
+          urlPattern: /^https:\/\/api\./i,
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api-cache',
+            cacheableResponse: {
+              statuses: [0, 200]
+            }
+          }
+        }
+      ],
+      cleanupOutdatedCaches: true
+    },
+    devOptions: {
+      enabled: true,
+      suppressWarnings: true,
+      type: 'module'
+    }
+  }
 })
